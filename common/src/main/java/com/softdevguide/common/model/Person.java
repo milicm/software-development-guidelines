a973200eec069f84210d3a3501de6a0da38180fc
Milos Milic <mmilic@gmail.com>
2018-05-01 20:34:41 +0200

    Added Person model class.

---
+++ common/src/main/java/com/softdevguide/common/model/Person.java
@@ -0,0 +1,85 @@
+package com.softdevguide.common.model;
+
+import java.io.Serializable;
+
+public class Person implements Serializable {
+
+	private static final long serialVersionUID = 5725941756560427483L;
+
+	private long id;
+	private String name;
+	private City city;
+
+	public Person() {
+		super();
+	}
+
+	public Person(String name, City city) {
+		super();
+		this.name = name;
+		this.city = city;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public City getCity() {
+		return city;
+	}
+
+	public void setCity(City city) {
+		this.city = city;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((city == null) ? 0 : city.hashCode());
+		result = prime * result + (int) (id ^ (id >>> 32));
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Person other = (Person) obj;
+		if (city == null) {
+			if (other.city != null)
+				return false;
+		} else if (!city.equals(other.city))
+			return false;
+		if (id != other.id)
+			return false;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "Person [id=" + id + ", name=" + name + ", city=" + city + "]";
+	}
+}
