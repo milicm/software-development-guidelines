6a4c03157c4e77563d27cdc8335a9d344c167f78
Milos Milic <mmilic@gmail.com>
2018-05-02 09:32:04 +0200

    CityRepository interface.

---
+++ general-principles/src/main/java/com/softdevguide/generalprinciples/repository/CityRepository.java
@@ -0,0 +1,11 @@
+package com.softdevguide.generalprinciples.repository;
+
+import com.softdevguide.common.model.City;
+
+public interface CityRepository {
+
+	void addCity(City city);
+
+	void removeCity(City city);
+
+}
