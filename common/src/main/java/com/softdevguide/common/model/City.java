5ecce908e7ae8b3728e922228158c7957ac6d5f5
Milos Milic <mmilic@gmail.com>
2018-07-15 09:53:56 +0200

    Added JPA entity annotations.

--- common/src/main/java/com/softdevguide/common/model/City.java
+++ common/src/main/java/com/softdevguide/common/model/City.java
@@ -2,11 +2,21 @@ package com.softdevguide.common.model;
 
 import java.io.Serializable;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class City implements Serializable {
 
 	private static final long serialVersionUID = 1185776441192211013L;
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private long id;
+	@Column
 	private String name;
 
 	public City() {
