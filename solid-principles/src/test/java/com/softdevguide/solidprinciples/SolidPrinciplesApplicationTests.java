0e4f4822dff3f61c8facb721849116f22438e28d
Milos Milic <mmilic@gmail.com>
2018-07-16 08:27:45 +0200

    SolidPrinciples base test class.

---
+++ solid-principles/src/test/java/com/softdevguide/solidprinciples/SolidPrinciplesApplicationTests.java
@@ -0,0 +1,16 @@
+package com.softdevguide.solidprinciples;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SolidPrinciplesApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
